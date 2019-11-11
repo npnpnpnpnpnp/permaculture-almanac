8b642e41add1aed993857919cffccd01946f03a1
dsdsdsdsdsds <dsdsdsdsdsds@users.noreply.github.com>
2019-11-11 08:51:46 +0100

    Revert to previous api.php template in order to get caching working

--- backend/dist/site-headless/templates/api.php
+++ backend/dist/site-headless/templates/api.php
@@ -1,4 +1,25 @@
 <?php namespace ProcessWire;
 
-// TODO: Why is the API called even if this line is commented out?
-$modules->get('RestApi')->callManually();
+// TODO: This doesn’t trigger the caching correctly but returns a 404
+// $modules->get('RestApi')->callManually();
+
+// Pre-check for unwanted symbols (hint from Ryan)
+if (strpos($input->urlSegmentStr(), '.') !== false) {
+  throw new Wire404Exception();
+}
+
+// Build request URL
+$protocol = $input->scheme() . "://";
+$endpoint = $modules->get("RestApi")->endpoint;
+$hostname = $config->httpHost;
+$url =  $protocol.$hostname."/".$endpoint."/".$input->urlSegmentStr();
+
+// This bit slows the traffic down a bit, but seems more secure
+$http = new WireHttp();
+$status = $http->status($url);
+if ($status === 200) {
+return $http->get($url);
+}
+
+// Prevent the creation of empty caches:
+throw new Wire404Exception();
