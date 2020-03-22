9cff48b0b641100ee50cea693a9f4fcf7b3b0f39
dsdsdsdsdsds <dsdsdsdsdsds@users.noreply.github.com>
2020-03-22 18:57:31 +0100

    Update dependencies and refine linting

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  //baseUrl: process.env.NODE_ENV === 'production' ? '/' : '/',
+  //publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   //assetsDir: 'assets',
   css: {
     sourceMap: true,
     loaderOptions: {
       sass: {
-        data: `
+        prependData: `
           @import '@/styles/globals.scss';
         `
       }
