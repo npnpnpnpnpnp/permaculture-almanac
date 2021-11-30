1da28373218402fba23a646f2082c383574d8b3f
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2021-11-30 15:37:34 +0100

    Fetch correct data of matrix types

--- backend/dist/site/templates/api/RepeaterMatrix.php
+++ backend/dist/site/templates/api/RepeaterMatrix.php
@@ -11,23 +11,51 @@ class RepeaterMatrix {
       $item = new \StdClass();
       $item->type = $matrixItem->type;
 
-      // if ($matrixItem->type === 'type_section') {
-      //   $title = $matrixItem->title;
-      //   if (!$title) continue;
-      //   $item->title = $title;
-      // }
+      if ($matrixItem->type === 'type_video') {
+        $item->title = $matrixItem->title;
+        $item->author = Helper::getPageReferences($matrixItem->author);
+        $item->url = $matrixItem->external_url;
+        $item->host = Helper::getPageReferences($matrixItem->host);
+      }
 
-      if ($matrixItem->type === 'type_body') {
+      if ($matrixItem->type === 'type_publication') {
+        $item->title = $matrixItem->title;
+        $item->author = Helper::getPageReferences($matrixItem->author);
+        $item->publisher = $matrixItem->publisher;
+        $item->isbn = $matrixItem->isbn;
+        $item->url = $matrixItem->external_url;
+      }
+
+      if ($matrixItem->type === 'type_person') {
+        $item->author = Helper::getPageReferences($matrixItem->author);
+        $item->url = $matrixItem->external_url;
+      }
+
+      if ($matrixItem->type === 'type_podcast') {
+        $item->title = $matrixItem->title;
+        $item->author = Helper::getPageReferences($matrixItem->author);
+        $item->url = $matrixItem->external_url;
+      }
+
+      if ($matrixItem->type === 'institution') {
+        $item->title = $matrixItem->title;
         $body = $matrixItem->body;
         if (!$body) continue;
         $item->body = Helper::convertToAbsolutePaths($body);
+        $item->url = $matrixItem->external_url;
       }
 
-      if ($matrixItem->type === 'type_images') {
-        $images = $matrixItem->images;
-        if (!$images->count) continue;
-        $item->images = Images::get($images);
-      }
+      // if ($matrixItem->type === 'type_body') {
+      //   $body = $matrixItem->body;
+      //   if (!$body) continue;
+      //   $item->body = Helper::convertToAbsolutePaths($body);
+      // }
+
+      // if ($matrixItem->type === 'type_images') {
+      //   $images = $matrixItem->images;
+      //   if (!$images->count) continue;
+      //   $item->images = Images::get($images);
+      // }
 
       // if ($matrixItem->type === 'type_image') {
       //   $image = $matrixItem->image;
@@ -41,11 +69,6 @@ class RepeaterMatrix {
       //   $item->body = Helper::getRepeater($list);
       // }
 
-      // if ($matrixItem->type === 'type_video') {
-      //   $mediaId = $matrixItem->media_id;
-      //   if (!$mediaId) continue;
-      //   $item->mediaId = $media_id;
-      // }
 
       array_push($matrix, $item);
     }
