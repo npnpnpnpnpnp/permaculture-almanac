652c99e96471bcf02d71d6a2772975a18231084c
npnpnpnpnpnp <19845311+npnpnpnpnpnp@users.noreply.github.com>
2022-02-02 09:20:04 +0100

    Rename form + update routes

--- backend/dist/site/templates/api/Routes.php
+++ backend/dist/site/templates/api/Routes.php
@@ -4,9 +4,9 @@ require_once wire('config')->paths->AppApi . "vendor/autoload.php";
 require_once wire('config')->paths->AppApi . "classes/AppApiHelper.php";
 
 require_once __DIR__ . "/Helper.php";
-
 require_once __DIR__ . "/Defaults.php";
 require_once __DIR__ . "/DefaultPage.php";
+require_once __DIR__ . "/SubmissionForm.php";
 
 $routes = [
   'v1' => [
@@ -19,8 +19,8 @@ $routes = [
       ['OPTIONS', '', ['GET']],
       ['GET', '{path:.+}', DefaultPage::class, 'get'],
       ['GET', '', DefaultPage::class, 'get'],
-      // ['OPTIONS', 'resource/submit', RestApiHelper::class, 'preflight', ['auth' => false]], // this is needed for CORS Requests
-      // ['POST', 'resource/submit', DonationForm::class, 'submissionForm']
+      // ['OPTIONS', 'resource/submit', AppApiHelper::class, 'preflight', ['auth' => false]], // this is needed for CORS Requests
+      // ['POST', 'resource/submit', SubmissionForm::class, 'submitForm']
     ]
   ]
 ];
